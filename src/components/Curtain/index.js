c05d19a98235032d4436e13cb8db9aec8636d19f
maruihan <phenix_infinity@hotmail.com>
2018-12-13 21:28:44 +0800

    add blur for me page

--- src/components/Curtain/index.js
+++ src/components/Curtain/index.js
@@ -1,22 +1,33 @@
 import React from "react";
 import {Text, View, Image} from "react-native";
 // Plugins
+import {BlurView} from "react-native-blur";
 // Style
 import style from "./style"
 
 export default class Curtain extends React.Component {
+  state = {
+    viewRef: null
+  };
+
   render() {
     const {name, avatar} = this.props;
     return (
       <View style={style.container}>
         <View style={style.underlay}>
-          <View style={style.underlayMask}/>
+          {/*<View style={style.underlayMask}/>*/}
           <Image style={style.underlayImage} source={
             avatar ?
               {uri: avatar}
               :
               require("../../assets/images/avatar_default.png")
           }/>
+          <BlurView
+            style={style.underlayMask}
+            viewRef={this.state.viewRef}
+            blurType="dark"
+            blurAmount={32}
+          />
         </View>
         <View style={style.content}>
           <View style={style.inner}>
